a1f043f5a86e940bb1f0f741c996c2a3f70d573e
Max Mönch <max.moench@hexfourty.com>
2023-01-08 12:27:16 +0100

    added sitemap and robots.txt

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -90,5 +90,14 @@ module.exports = {
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom'],
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+    [
+      'vuepress-plugin-sitemap',
+      {
+        hostname: 'https://docs.klingl.me',
+      },
+    ],
+  ],
 };
